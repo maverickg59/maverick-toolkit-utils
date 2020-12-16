2585f23fca3b23ead7e4b69b8f66b0a146157e13
Christopher White <christopher.evan.white88@gmail.com>
2020-12-15 21:54:27 -0700

    added kebabToCamel and chunkArray functions

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ export {
 } from './lib/named-export-structure'
 
 export { myNameIs } from './lib/default-export-structure'
+
+export { chunkArray } from './lib/array'
+
+export { kebabToCamel } from './lib/string'
