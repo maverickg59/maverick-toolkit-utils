e1e3ea367bfac88e8fabd803e1501f844db374e1
Christopher White <christopher.evan.white88@gmail.com>
2021-01-16 03:04:49 -0700

    updated chunk utility function

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ export {
   logFromFunctionDeclaration,
   logFromFunctionExpression,
 } from './lib/named-export-structure'
-
 export { myNameIs } from './lib/default-export-structure'
-
-export { chunkArray } from './lib/array'
-
+export { chunk } from './lib/array'
 export { kebabToCamel } from './lib/string'
-
 export { isMobile } from './lib/browser'
