6634da1133dd40bc74d5fc36486312f32c4c81dd
Christopher White <christopher.evan.white88@gmail.com>
2021-10-11 23:20:29 -0700

    updates to utility library

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-export {
-  logFromFunctionDeclaration,
-  logFromFunctionExpression,
-} from './lib/named-export-structure'
-export { myNameIs } from './lib/default-export-structure'
-export { chunk } from './lib/array'
-export { kebabToCamel } from './lib/string'
-export { isMobile } from './lib/browser'
+export * from './utilities'
